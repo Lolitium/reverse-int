5d80fc4e8dade6624b62ccb7a4facbf89e7945e9
Ekaterina Mikhailova <ekaterinavorobyova20@gmail.com>
2023-01-04 11:43:00 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let result='';
+    let i=0;
+    while (i<n.toString().length) {
+        result = `${n.toString()[i]}${result}`;
+        i=i+1;
+    }
+    return parseInt(result);
 }
